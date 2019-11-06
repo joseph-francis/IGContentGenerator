8b2b1396bdb00c6f556672e308468d80fe66e2b7
Joseph Francis <josephfrancis100@gmail.com>
2019-11-05 22:23:21 -0600

    Size modification

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas
 from ImageText import ImageText
 
-IGSize = (1080, 1350)
+IGSize = (1080, 1080)
 
 with open("quotes.txt") as quotes:
     all_quotes = quotes.read()
@@ -22,16 +22,16 @@ quotes_array = all_quotes.split("\n\n")
 font_name = "SFUIText-Bold.ttf"
 medium_font = "SFUIText-Medium.ttf"
 white_color = (255, 255, 255)
-quote_coordinates = (40, IGSize[1]/2 - 150)
-hest_name_coordinate = (40, IGSize[1] - 150)
-sub_coordinate = (40, IGSize[1] - 120)
+quote_coordinates = (40, IGSize[1]/2 - 200)
+hest_name_coordinate = (40, IGSize[1] - 170)
+sub_coordinate = (40, IGSize[1] - 130)
 
 
 for (index, quote) in enumerate(quotes_array):
     img = ImageText(IGSize, background=colors[index])
-    img.write_text_box(quote_coordinates, quote, font_filename=font_name, font_size=49, box_width=IGSize[0] * 0.8, color=white_color)
-    img.write_text_box(hest_name_coordinate, "Hest", font_filename=font_name, font_size=30, box_width=500, color=white_color)
-    img.write_text_box(sub_coordinate, "Available on the App Store", font_filename=medium_font, font_size=30, box_width=500,
+    img.write_text_box(quote_coordinates, quote, font_filename=font_name, font_size=54, box_width=IGSize[0] * 0.8, color=white_color)
+    img.write_text_box(hest_name_coordinate, "Hest", font_filename=font_name, font_size=40, box_width=700, color=white_color)
+    img.write_text_box(sub_coordinate, "Available on the App Store", font_filename=medium_font, font_size=40, box_width=700,
                        color=white_color)
     img.save(f"../../IGContent/{index}.png")
     print(f"Image saved with filename: {index}.png")
